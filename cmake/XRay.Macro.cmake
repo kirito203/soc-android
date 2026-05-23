1350ad63de1e7d7bca32aea9011bccf83dcc3682
Kirito203 <132993434+Kirito203@users.noreply.github.com>
2026-05-23 14:51:20 +0300

    решил наконец cmake

--- cmake/XRay.Macro.cmake
+++ cmake/XRay.Macro.cmake
@@ -1,8 +1,18 @@
-function(target_sources_grouped)
-    set(TARGET_NAME ${ARGV0})
-    list(REMOVE_AT ARGV 0)
+macro(target_sources_grouped TARGET)
+    # Защищаем парсер от раскрытия символов \C на Windows.
+    # Превращаем аргументы в чистую строку, экранируя кавычки.
+    string(CONFIGURE "${ARGN}" SAFE_ARGS ESCAPE_QUOTES)
+    
+    # Заменяем виндовые бэкслеши на нормальные слэши прямо в тексте
+    string(REPLACE "\\" "/" SAFE_ARGS "${SAFE_ARGS}")
+    
+    # Вырезаем блоки группировки Visual Studio (NAME ... FILES)
+    string(REGEX REPLACE "NAME;[^;]+;FILES;" "" CLEAN_FILES "${SAFE_ARGS}")
+    string(REGEX REPLACE "NAME;[^;]+" "" CLEAN_FILES "${CLEAN_FILES}")
+    string(REPLACE "FILES;" "" CLEAN_FILES "${CLEAN_FILES}")
 
-    if(TARGET ${TARGET_NAME})
-        target_sources(${TARGET_NAME} PRIVATE ${ARGV})
+    # Скармливаем чистые файлы компилятору
+    if(CLEAN_FILES)
+        target_sources(${TARGET} PRIVATE ${CLEAN_FILES})
     endif()
-endfunction()
+endmacro()
